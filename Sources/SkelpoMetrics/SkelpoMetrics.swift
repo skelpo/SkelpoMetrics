79aa80555bb3b6f3dbc758d9f0cb8f4d003f1dd5
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-22 07:39:22 -0500

    Created Config and Error structs

--- Sources/SkelpoMetrics/SkelpoMetrics.swift
+++ Sources/SkelpoMetrics/SkelpoMetrics.swift
@@ -1,1 +1,39 @@
+import Foundation
 
+public struct Error: Swift.Error {
+    public let identifier: String
+    public let reason: String
+
+    public init(identifier: String, reason: String) {
+        self.identifier = identifier
+        self.reason = reason
+    }
+}
+
+public struct Config {
+    public let url: String
+    public let key: String
+    public let id: String
+
+    public init(url: String, key: String, id: String) {
+        self.url = url
+        self.key = key
+        self.id = id
+    }
+
+    public init(url: String? = nil)throws {
+        guard let url = url ?? ProcessInfo.processInfo.environment["SKELPO_METRICS_URL"] else {
+            throw Error(identifier: "missingEnvVar", reason: "Could not get value for `SKELPO_METRICS_URL` env var")
+        }
+        guard let key = ProcessInfo.processInfo.environment["SKELPO_METRICS_KEY"] else {
+            throw Error(identifier: "missingEnvVar", reason: "Could not get value for `SKELPO_METRICS_KEY` env var")
+        }
+        guard let id = ProcessInfo.processInfo.environment["SKELPO_METRICS_ID"] else {
+            throw Error(identifier: "missingEnvVar", reason: "Could not get value for `SKELPO_METRICS_ID` env var")
+        }
+
+        self.url = url
+        self.key = key
+        self.id = id
+    }
+}
