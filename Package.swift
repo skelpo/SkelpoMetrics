fbf3c216dd86f7a0ed6a39f49f493cbc2f8bdc04
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-25 11:59:08 -0500

    Installed swift-log package

--- Package.swift
+++ Package.swift
@@ -8,11 +8,12 @@ let package = Package(
         .library(name: "SkelpoMetrics", targets: ["SkelpoMetrics"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
         .package(url: "https://github.com/apple/swift-metrics", from: "1.0.0-convergence.1")
     ],
     targets: [
-        .target(name: "SkelpoMetrics", dependencies: ["Metrics"]),
+        .target(name: "SkelpoMetrics", dependencies: ["Metrics", "Logging"]),
         .target(name: "VaporSkelpoMetrics", dependencies: ["SkelpoMetrics", "Vapor"]),
         .testTarget(name: "SkelpoMetricsTests", dependencies: ["SkelpoMetrics"]),
     ]
