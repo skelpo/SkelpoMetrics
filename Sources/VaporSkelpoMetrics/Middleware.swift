c027a4ffca99141d5763e723cfc2edf49c7ffb2e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-25 10:56:10 -0500

    Documented types and extensions in VaporSkelpoMetrics target

--- Sources/VaporSkelpoMetrics/Middleware.swift
+++ Sources/VaporSkelpoMetrics/Middleware.swift
@@ -1,14 +1,34 @@
 import SkelpoMetrics
 import Vapor
 
-public final class SkelpoMetricsMiddleware: Middleware {
-    public init() { }
+/// A middleware that stores metric data from an API call.
+///
+/// For this middleware to work, you will need to register `SwiftMetrics.Config` as
+/// a service with your app's `Services` instance.
+///
+/// This middleware can be register either as a single instance, or an instance per event-loop:
+///
+///     services.register(SkelpoMetricsMiddleware())
+///     services.register(SkelpoMetricsMiddleware.self)
+public final class SkelpoMetricsMiddleware: Middleware, ServiceType {
 
+    /// See `ServiceType.makeService(for:)`.
+    public static func makeService(for container: Container) throws -> SkelpoMetricsMiddleware {
+        return try SkelpoMetricsMiddleware(config: container.make(), logger: container.make(), client: container.make())
+    }
+
+    private let config: SkelpoMetrics.Config
+    private let logger: Logger
+    private let client: Client
+
+    private init(config: SkelpoMetrics.Config, logger: Logger, client: Client) {
+        self.config = config
+        self.logger = logger
+        self.client = client
+    }
+
+    /// See `Middleware.respond(to:chainingTo:)`.
     public func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
-        let config = try request.make(SkelpoMetrics.Config.self)
-        let logger = try request.make(Logger.self)
-        let client = try request.make(Client.self)
-
         var event = Event(type: "request")
         return try next.respond(to: request).flatMap { response in
             event.attributes["status"] = response.http.status.code.description
@@ -18,13 +38,13 @@ public final class SkelpoMetricsMiddleware: Middleware {
             let body: Data
             do { body = try JSONEncoder().encode(event) }
             catch let error {
-                logger.error("Metric event failed to save with error: \(error.localizedDescription)")
+                self.logger.error("Metric event failed to save with error: \(error.localizedDescription)")
                 return request.future(response)
             }
 
-            let http = HTTPRequest(method: .POST, url: config.url, headers: ["Authorization": config.key], body: body)
-            return client.send(Request(http: http, using: request)).transform(to: response).catchMap { error in
-                logger.error("Metric event failed to save with error: \(error.localizedDescription)")
+            let http = HTTPRequest(method: .POST, url: self.config.url, headers: ["Authorization": self.config.key], body: body)
+            return self.client.send(Request(http: http, using: request)).transform(to: response).catchMap { error in
+                self.logger.error("Metric event failed to save with error: \(error.localizedDescription)")
                 return response
             }
         }
--- Sources/VaporSkelpoMetrics/SkelpoMetrics+Vapor.swift
+++ Sources/VaporSkelpoMetrics/SkelpoMetrics+Vapor.swift
@@ -2,21 +2,32 @@
 import Vapor
 
 extension SkelpoMetrics.Error: AbortError {
+
+    /// See `AbortError.HTTPResponseStatus`.
     public var status: HTTPResponseStatus {
         switch self.identifier {
-        case "missingEnvVar": return .internalServerError
         default: return .internalServerError
         }
     }
 }
 
 extension SkelpoMetrics.Config: ServiceType {
+
+    /// See `ServiceType.makeService(for:)`.
     public static func makeService(for container: Container) throws -> SkelpoMetrics.Config {
         return try .init()
     }
 }
 
 extension SkelpoMetrics.SkelpoMetric {
+
+    /// Saves a guage's value to the configured metric API on an event-loop.
+    ///
+    /// - Parameters:
+    ///   - guage: The guage whos data will be saved to the API.
+    ///   - eventLoop: The event-loop that the future which is returned will live on.
+    ///
+    /// - Returns: A void `EventLoopFuture`, which succeedes when the storage completes.
     public func record(_ guage: SkelpoMetricFactory.Guage, on eventLoop: EventLoop) -> EventLoopFuture<Void> {
         let promise = eventLoop.newPromise(Void.self)
         self.record(guage) { result in
@@ -28,6 +39,13 @@ extension SkelpoMetrics.SkelpoMetric {
         return promise.futureResult
     }
 
+    /// Saves a timer's durations to the configured metric API on an event-loop.
+    ///
+    /// - Parameters:
+    ///   - timer: The timer whos data will be saved to the API.
+    ///   - eventLoop: The event-loop that the future which is returned will live on.
+    ///
+    /// - Returns: A void `EventLoopFuture`, which succeedes when the storage completes.
     public func record(_ timer: SkelpoMetricFactory.Timer, on eventLoop: EventLoop) -> EventLoopFuture<Void> {
         let promise = eventLoop.newPromise(Void.self)
         self.record(timer) { result in
@@ -39,6 +57,13 @@ extension SkelpoMetrics.SkelpoMetric {
         return promise.futureResult
     }
 
+    /// Saves a counter's value to the configured metric API on an event-loop.
+    ///
+    /// - Parameters:
+    ///   - counter: The counter whos data will be saved to the API.
+    ///   - eventLoop: The event-loop that the future which is returned will live on.
+    ///
+    /// - Returns: A void `EventLoopFuture`, which succeedes when the storage completes.
     public func record(_ counter: SkelpoMetricFactory.Counter, on eventLoop: EventLoop) -> EventLoopFuture<Void> {
         let promise = eventLoop.newPromise(Void.self)
         self.record(counter) { result in
@@ -50,6 +75,13 @@ extension SkelpoMetrics.SkelpoMetric {
         return promise.futureResult
     }
 
+    /// Saves a recorder's values to the configured metric API on an event-loop.
+    ///
+    /// - Parameters:
+    ///   - recorder: The recorder whos data will be saved to the API.
+    ///   - eventLoop: The event-loop that the future which is returned will live on.
+    ///
+    /// - Returns: A void `EventLoopFuture`, which succeedes when the storage completes.
     public func record(_ recorder: SkelpoMetricFactory.Recorder, on eventLoop: EventLoop) -> EventLoopFuture<Void> {
         let promise = eventLoop.newPromise(Void.self)
         self.record(recorder) { result in
