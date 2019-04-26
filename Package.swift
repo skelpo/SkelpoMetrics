0531096946941822a81277133ae7861728f09a35
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-26 07:29:04 -0500

    Removed swift-log dependency
    This dependency conflicts with vapor/console@3.0.0, so we can't use it

--- Package.swift
+++ Package.swift
@@ -8,12 +8,11 @@ let package = Package(
         .library(name: "SkelpoMetrics", targets: ["SkelpoMetrics"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
         .package(url: "https://github.com/apple/swift-metrics", from: "1.0.0-convergence.1")
     ],
     targets: [
-        .target(name: "SkelpoMetrics", dependencies: ["Metrics", "Logging"]),
+        .target(name: "SkelpoMetrics", dependencies: ["Metrics"]),
         .target(name: "VaporSkelpoMetrics", dependencies: ["SkelpoMetrics", "Vapor"]),
         .testTarget(name: "SkelpoMetricsTests", dependencies: ["SkelpoMetrics"]),
     ]
